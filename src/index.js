cccd7e3621b1d6e458457c3da827727cfae0b5f2
Ridwan <ridwan>
2022-02-23 15:16:42 +0000

    Fix errors found by linter

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import "./style.css";
-import TaskList, { list, inputTodo } from "./modules/taskList.js";
+import './style.css';
+import TaskList, { list, inputTodo } from './modules/taskList.js';
 
-const addTodoBtn = document.querySelector(".submit");
-const clearList = document.querySelector(".clear");
+const addTodoBtn = document.querySelector('.submit');
+const clearList = document.querySelector('.clear');
 
 const myTasks = new TaskList();
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   myTasks.getFromLocal(myTasks.tasks);
   myTasks.renderTodo(list);
 });
 
-addTodoBtn.addEventListener("click", (e) => {
+addTodoBtn.addEventListener('click', (e) => {
   e.preventDefault();
   myTasks.addTodo(inputTodo.value, myTasks.tasks);
   myTasks.renderTodo(list);
   myTasks.saveTolocal();
 });
 
-clearList.addEventListener("click", () => {
+clearList.addEventListener('click', () => {
   myTasks.clearCompleted();
   myTasks.saveTolocal();
   myTasks.renderTodo(list);
