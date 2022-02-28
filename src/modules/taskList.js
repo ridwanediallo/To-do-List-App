8de78b1771e6663cb22577bf980b752df7881430
ridwan <ridwanediallo99@gmail.com>
2022-02-28 08:41:42 +0000

    Update readme

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -1,7 +1,7 @@
-import TodoTask from './todoTask.js';
+import TodoTask from "./todoTask.js";
 
-export const list = document.querySelector('.list');
-export const inputTodo = document.querySelector('.add-todo');
+export const list = document.querySelector(".list");
+export const inputTodo = document.querySelector(".add-todo");
 
 export default class TaskList {
   constructor() {
@@ -9,26 +9,26 @@ export default class TaskList {
   }
 
   saveTolocal() {
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 
   getFromLocal = () => {
-    this.tasks = localStorage.getItem('tasks')
-      ? JSON.parse(localStorage.getItem('tasks'))
+    this.tasks = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
       : [];
   };
 
   renderTodo = (list) => {
-    list.innerHTML = '';
+    list.innerHTML = "";
     this.tasks.forEach((el, i) => {
-      const item = document.createElement('li');
-      item.classList.add('item');
+      const item = document.createElement("li");
+      item.classList.add("item");
       item.id = i;
       item.innerHTML = `
             <div class="left-itmes">
-            <input type="checkbox" class="checkbox" ${el.done ? 'checked' : ''}>
+            <input type="checkbox" class="checkbox" ${el.done ? "checked" : ""}>
             <input type='text' class='todo ${
-              el.done ? 'line-through' : ''
+              el.done ? "line-through" : ""
             }' value='${el.desp}'>
             </div>
             <div>
@@ -37,35 +37,35 @@ export default class TaskList {
             </div>
       `;
 
-      item.addEventListener('click', () => {
-        const iconRemove = item.querySelector('.icon-remove');
-        const iconDots = item.querySelector('.icon-dots');
-        iconRemove.classList.toggle('hidden');
-        iconDots.classList.toggle('hidden');
-        iconRemove.addEventListener('click', (event) => {
+      item.addEventListener("click", () => {
+        const iconRemove = item.querySelector(".icon-remove");
+        const iconDots = item.querySelector(".icon-dots");
+        iconRemove.classList.toggle("hidden");
+        iconDots.classList.toggle("hidden");
+        iconRemove.addEventListener("click", (event) => {
           const taskItem = event.target.parentNode.parentNode;
           this.removeItem(taskItem);
         });
       });
 
-      item.style.borderBottom = '1px solid #aaa';
+      item.style.borderBottom = "1px solid #aaa";
 
-      const checkBox = item.querySelector('.checkbox');
-      const todo = item.querySelector('.todo');
-      todo.classList.add('todo-style');
-      checkBox.addEventListener('click', this.clickCheck);
+      const checkBox = item.querySelector(".checkbox");
+      const todo = item.querySelector(".todo");
+      todo.classList.add("todo-style");
+      checkBox.addEventListener("click", this.clickCheck);
 
       list.append(item);
     });
   };
 
   addTodo(desp, tasks) {
-    if (desp !== '') {
+    if (desp !== "") {
       const todoItems = new TodoTask(desp, tasks);
       this.tasks.push(todoItems);
     }
 
-    inputTodo.value = '';
+    inputTodo.value = "";
   }
 
   clickCheck = (e) => {
@@ -73,13 +73,14 @@ export default class TaskList {
     const div = e.target.parentNode;
     const { id } = div.parentNode;
     this.tasks[id].done = !this.tasks[id].done;
-    todo.classList.toggle('line-through');
+    todo.classList.toggle("line-through");
     this.saveTolocal();
   };
 
   removeFromLocal(index) {
     this.tasks = this.tasks.filter((task) => +task.index !== +index);
     this.tasks.forEach((el, i) => {
+      console.log(el);
       el.index = i;
     });
     this.saveTolocal();
